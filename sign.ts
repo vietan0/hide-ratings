7c3f9e7d4b7053ad27aa56359f8d8137f1ffb933
Viet An <tiktaktictac269@gmail.com>
2025-08-19 20:59:45 +0700

    chore: publish v1.6.1

--- sign.ts
+++ sign.ts
@@ -17,7 +17,6 @@ function parseArgs(args: Record<string, string | undefined>) {
 
 const args = {
   '--channel': 'listed',
-  '--amo-metadata': 'metadata.json',
   '--api-key': process.env.WEB_EXT_API_KEY,
   '--api-secret': process.env.WEB_EXT_API_SECRET,
 };
