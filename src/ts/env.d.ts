f6f3927e48f8307045c42756e30fc7613a135efc
Viet An <tiktaktictac269@gmail.com>
2025-09-05 04:12:24 +0700

    build: add script for uploading to chrome store

--- src/ts/env.d.ts
+++ src/ts/env.d.ts
@@ -3,6 +3,7 @@ declare namespace NodeJS {
     readonly WEB_EXT_API_KEY: string;
     readonly WEB_EXT_API_SECRET: string;
     readonly CHROMIUM_PROFILE: string;
+    readonly EXTENSION_ID: string;
     readonly CLIENT_ID: string;
     readonly CLIENT_SECRET: string;
     readonly REFRESH_TOKEN: string;
